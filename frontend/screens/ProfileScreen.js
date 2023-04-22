f795f1e4ecaf94653f1aa73b4a0b3ec05ff291d1
Ian Ng <ianng@Ians-MacBook-Air.local>
2023-04-22 02:19:47 -0700

    added assets & screens

--- frontend/screens/ProfileScreen.js
+++ frontend/screens/ProfileScreen.js
@@ -1,7 +1,89 @@
-import { Text } from "react-native";
+import React from 'react'
+import { Image, SafeAreaView, StyleSheet, View } from 'react-native'
+import { Divider, Icon, Text } from 'react-native-elements'
+import Layout from '../constants/Layout'
+import { HomeScreenPics } from '../constants/Pics'
+import { randomNo } from '../utils/randomNo' //####Remove after connecting with database
 
-export default function ProfileScreen() {
+const { pic, title } = HomeScreenPics[randomNo(1, HomeScreenPics.length)]
+
+const Social = ({ name }) => (
+  <Icon
+    name={name}
+    type="font-awesome"
+    containerStyle={styles.iconContainer}
+    size={32}
+  />
+)
+
+class ProfileScreen extends React.Component {
+  render() {
     return (
-        <Text>This is the Profile Screen</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image source={pic} style={styles.image} />
+        </View>
+        <Text h4 style={styles.name}>
+          {title}
+        </Text>
+        <Text style={styles.desc}>Sophomore at UCLA</Text>
+        <Divider style={styles.divider} />
+        <Text style={styles.desc}>
+          I am a scholar, i have 30 hours of coaching
+        </Text>
+        <Divider style={styles.divider} />
+        <Text style={styles.desc}>Catch me below</Text>
+        <View style={styles.socialLinks}>
+          <Social name="linkedin" />
+          <Social name="instagram" />
+          <Social name="facebook-square" />
+        </View>
+      </SafeAreaView>
     )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    margin: 20,
+  },
+  image: {
+    width: Layout.window.width - 60,
+    height: Layout.window.height / 2 - 60,
+    borderRadius: 20,
+  },
+  name: {
+    color: '#5E5E5E',
+    alignSelf: 'flex-start',
+    marginLeft: 30,
+  },
+  desc: {
+    color: '#5E5E5E',
+    alignSelf: 'flex-start',
+    marginTop: 5,
+    marginHorizontal: 30,
+    fontSize: 14,
+  },
+  divider: {
+    backgroundColor: '#C0C0C0',
+    width: Layout.window.width - 60,
+    margin: 20,
+  },
+  socialLinks: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    width: Layout.window.width,
+    marginLeft: 40,
+  },
+  iconContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 15,
+  },
+})
+
+export default ProfileScreen
