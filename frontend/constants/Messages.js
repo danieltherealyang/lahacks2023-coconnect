eff413cc397d0d1581cc07c6a93e7e9be2c65e4a
Ian Ng <ianng@Ians-MacBook-Air.local>
2023-04-22 02:53:35 -0700

    constants

---
+++ frontend/constants/Messages.js
@@ -0,0 +1,6 @@
+export const Messages = 
+    {
+      pic: require('../assets/images/women/women1.jpg'),
+      title: 'Jenny, 12',
+      message: "12 o' Clock at starbuck",
+    }
---
+++ frontend/constants/Pics.js
@@ -0,0 +1,13 @@
+export const HomeScreenPics = [
+{
+    pic: require('../assets/images/women/women1.jpg'),
+    title: 'Jenn, UCLA',
+    caption: '3.9 GPA',
+},
+{
+    pic: require('../assets/images/women/women1.jpg'),
+    title: 'Xenn, USC',
+    caption: '3.5 GPA',
+}
+]
+
